058639c1df7efc502fa17e7390e42a768f643772
lburgazzoli <lburgazzoli@gmail.com>
2013-09-04 17:51:15 +0200

    project cleanup

--- quickfixj-transport-netty/src/main/java/quickfix/transport/netty/test/FIXInitiatorMain.java
+++ quickfixj-transport-netty/src/main/java/quickfix/transport/netty/test/FIXInitiatorMain.java
@@ -39,22 +39,52 @@ import quickfix.transport.netty.FIXSocketInitiator;
  *
  */
 public class FIXInitiatorMain {
-    private static final Logger LOGGEGR =
-        LoggerFactory.getLogger(FIXInitiatorMain.class);
+    private static final Logger LOGGEGR = LoggerFactory.getLogger(FIXInitiatorMain.class);
 
+    /**
+     *
+     * @param sid
+     * @return
+     */
+    public static SessionSettings getSettingsFor(SessionID sid) {
+        SessionSettings cfg  = new SessionSettings();
+        cfg.setString(sid,"ConnectionType","initiator");
+        cfg.setString(sid,"BeginString","FIX.4.2");
+        cfg.setString(sid,"SenderCompID","TEXT");
+        cfg.setString(sid,"TargetCompID","EXEC");
+        cfg.setString(sid,"ReconnectInterval","30");
+        cfg.setString(sid,"HeartBtInt","30");
+        cfg.setString(sid,"SocketConnectPort","6543");
+        cfg.setString(sid,"SocketConnectHost","localhost");
+        cfg.setString(sid,"StartTime","00:00:00");
+        cfg.setString(sid,"EndTime","23:59:00");
+        cfg.setString(sid,"MillisecondsInTimeStamp","Y");
+        cfg.setString(sid,"UseDataDictionary","N");
+        cfg.setString(sid,"ResetOnLogon","Y");
+
+        return cfg;
+    }
+
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         try {
+
+            SessionID             sid  = new SessionID("FIX.4.2","TEST","EXEC");
+            SessionSettings       cfg  = getSettingsFor(sid);
             Application           app  = new FIXApplication();
-            SessionSettings       cfg  = new SessionSettings("etc/initiator.cfg");
             MessageStoreFactory   msf  = new MemoryStoreFactory();
             LogFactory            logf = new SLF4JLogFactory(cfg);
             MessageFactory        msgf = new DefaultMessageFactory();
             DefaultSessionFactory dsf  = new DefaultSessionFactory(app,msf,logf,msgf);
-            SessionID             sid  = new SessionID("FIX.4.2","BANZAI","EXEC");
             FIXRuntime            rt   = new FIXRuntime();
-            FIXSession            sx   = new FIXSession(rt,dsf.create(sid,cfg));
 
-            new FIXSocketInitiator(rt,sx,"localhost",9878).run();
+            new FIXSocketInitiator(
+                rt,
+                new FIXSession(rt,dsf.create(sid,cfg)),
+                cfg.getString(sid,"SocketConnectHost"),
+                cfg.getInt(sid,"SocketConnectPort")).run();
 
         } catch(Exception e) {
             LOGGEGR.warn("Exception",e);
