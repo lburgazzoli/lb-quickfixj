f309e87f7fd7c9f68ed934673767215a3c30b869
lburgazzoli <lburgazzoli@gmail.com>
2014-01-01 21:46:24 +0100

    fix license

--- quickfixj-transport-netty/src/main/java/com/github/lburgazzoli/quickfixj/transport/netty/codec/NettyRegExMessageDecoder.java
+++ quickfixj-transport-netty/src/main/java/com/github/lburgazzoli/quickfixj/transport/netty/codec/NettyRegExMessageDecoder.java
@@ -1,22 +1,18 @@
-/*******************************************************************************
- * Copyright (c) quickfixengine.org  All rights reserved.
+/*
+ * Copyright 2014 lb
  *
- * This file is part of the QuickFIX FIX Engine
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This file may be distributed under the terms of the quickfixengine.org
- * license as defined by quickfixengine.org and appearing in the file
- * LICENSE included in the packaging of this file.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
- * THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A
- * PARTICULAR PURPOSE.
- *
- * See http://www.quickfixengine.org/LICENSE for licensing information.
- *
- * Contact ask@quickfixengine.org if any conditions of this licensing
- * are not clear to you.
- ******************************************************************************/
-
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.lburgazzoli.quickfixj.transport.netty.codec;
 
 import com.github.lburgazzoli.quickfixj.transport.FIXCodecHelper;
