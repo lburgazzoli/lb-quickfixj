9458b9696e47aed35e9357b31a58f31969a4a104
lburgazzoli <lburgazzoli@gmail.com>
2014-01-04 12:10:16 +0100

    fix quickfixj-transport-netty decoder

--- quickfixj-transport-netty/src/main/java/com/github/lburgazzoli/quickfixj/transport/netty/codec/NettyMessageDecoder.java
+++ quickfixj-transport-netty/src/main/java/com/github/lburgazzoli/quickfixj/transport/netty/codec/NettyMessageDecoder.java
@@ -19,8 +19,6 @@ import com.github.lburgazzoli.quickfixj.transport.FIXCodecHelper;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
